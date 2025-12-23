b2cd23aed7e4bc4883277918e499bee3e877e2c1
burkush <avburkush@gmail.com>
2025-12-23 20:13:43 +0200

    Set up the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button>Click</Button>
+    </div>
+  );
+};
+
+export default App;
